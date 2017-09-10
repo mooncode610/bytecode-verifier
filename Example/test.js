5ed26780e27734ef7b1eeee7f4c2e25c1b4f8489
Mooni Don <mooncode610@gmail.com>
2017-09-10 23:22:00 +0800

    verifier working with comments and loadRemoteVersion

--- Example/test.js
+++ Example/test.js
@@ -1,12 +1,11 @@
 const solc = require('solc');
-
-solc.loadRemoteVersion('v0.4.11-nightly.2017.3.29+commit.fefb3fad', function(err, solcV) {
+var test = 'hello!'
+solc.loadRemoteVersion('v0.4.10+commit.f0d539ae', function(err, solcV) {
     if (err) {
         // An error was encountered, display and quit
         console.log(err)
     }
-
-    var output = solcV.compile("contract t { function g() {} }", 0, (output) => {
-        console.log(output);
-    });
+    console.log(solcV)
+    var output = solcV.compile("contract t { function g() {} }", 1);
+    console.log(output);
 });
